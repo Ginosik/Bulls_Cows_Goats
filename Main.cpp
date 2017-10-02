207f3d79d73569bc7d6032c9476be30a0f04f0a8
Ginosik <luis.ega@hotmail.com>
2017-10-02 11:34:06 -0300

    Entering guess

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	constexpr int WORD_LENGTH = 5;
+	//Introducing the game
+	constexpr int WORD_LENGTH = 9;
 	cout << "Welcome to Bulls and Cows, a fun word game." << endl;
-	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";
+	cout << "Can you guess the " << WORD_LENGTH;
+	cout << " letter isogram I'm thinking of?\n";
+	cout << endl;
+
+	//Get a guess from the player
+	cout << "What will be your guess?";
+	string Guess = "";
+	cin >> Guess;
+
+	//Repeat the guess back to them
+	cout << "Your guess was: " << Guess << endl;
+
+	cout << endl;
+
 	return 0;
 }
